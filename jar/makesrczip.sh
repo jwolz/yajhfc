212b3330bacccafba79fee1c8078530f2956d4f4
Jonas Wolz <jonas@yajhfc.de>
2006-12-28 20:54:08 +0100

    Added script to make a "clean" source archive

---
+++ jar/makesrczip.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Creates a source archive in the parent directory of the yajhfc tree
+
+cd ../..
+rm -f yajhfc.zip
+zip -r yajhfc.zip yajhfc/  -x 'yajhfc/yajhfc/*.class' -x 'yajhfc/jar/yajhfc.jar' -x 'yajhfc/doc/faq-src/*'  -x '*/CVS/*' -x '*~'
+zip yajhfc.zip yajhfc/doc/faq-src/.[!.]* yajhfc/doc/faq-src/*.tex yajhfc/doc/faq-src/*.sh
+
