29198e57ef900db509ab65094194905246319a3b
Jonas Wolz <jonas@yajhfc.de>
2008-10-26 13:10:19 +0100

    Updated translations

--- jar/makesrczip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Creates a source archive in the parent directory of the yajhfc tree
-
-cd ../..
-rm -f yajhfc.zip
-zip -r yajhfc.zip yajhfc/  -x 'yajhfc/build/*' -x 'yajhfc/yajhfc/*.class' -x 'yajhfc/jar/yajhfc.jar' -x 'yajhfc/doc/faq-src/*'  -x '*/CVS/*' -x '*~'
-zip yajhfc.zip yajhfc/doc/faq-src/.[!.]* yajhfc/doc/faq-src/*.tex yajhfc/doc/faq-src/*.sh yajhfc/yajhfc/i18n/*.class
-
