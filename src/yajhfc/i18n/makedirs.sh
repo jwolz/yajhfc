c006df606daaa7eb077197ee92448454c83d59cb
Jonas Wolz <jonas@yajhfc.de>
2010-05-01 09:13:14 +0200

    Some changes

---
+++ src/yajhfc/i18n/makedirs.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Moves the translation into directories named after the language code
+
+if [ -z $1 ]; then
+  TARGETDIR=/tmp/yajhfc-translation
+else
+  TARGETDIR=$1
+fi
+
+if [ ! -d $TARGETDIR ]; then
+  mkdir -p $TARGETDIR
+fi
+
+# Usage copyfile FILE
+copyfile() {
+	PO=$1
+  	LANG=${PO##*_}
+  	LANG=${LANG%.po}
+	
+	if [ ! -d $TARGETDIR/$LANG ]; then
+	  mkdir $TARGETDIR/$LANG 
+	fi
+
+	cp $PO $TARGETDIR/$LANG/
+}
+
+for PO in messages_*.po CommandLineOpts_*.po UIDefaults_*.po ; do
+	copyfile $PO
+done 
+
+mkdir $TARGETDIR/templates
+cp messages.po $TARGETDIR/templates/messages.pot
+cp UIDefaults.po $TARGETDIR/templates/UIDefaults.pot
+perl maketemplate.pl < CommandLineOpts.po > $TARGETDIR/templates/CommandLineOpts.pot
+
+mkdir $TARGETDIR/en
+cp CommandLineOpts.po $TARGETDIR/en
+
+
+
+
+
+
