a6d73e8c65ecf43fee16371302e9b6565d4c904c
jonas <jonas@yajhfc.de>
2011-10-25 09:54:53 +0200

    Updated license to GPLv3

--- src/yajhfc/i18n/jxgettext.sh
+++ src/yajhfc/i18n/jxgettext.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Extracts all translatable strings into messages.po
 
-xgettext -k_ --from-code=utf-8 `find .. -name '*.java'`
+find .. -name '*.java' | xgettext -k_ --from-code=utf-8 -f-
 
