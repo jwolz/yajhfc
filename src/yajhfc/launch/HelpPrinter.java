2268c0d9f60b027e664bff1263b848bec862c3ce
jonas <jonas@yajhfc.de>
2011-11-20 10:13:29 +0100

    Added license exception

--- src/yajhfc/launch/HelpPrinter.java
+++ src/yajhfc/launch/HelpPrinter.java
@@ -14,6 +14,25 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *  Linking YajHFC statically or dynamically with other modules is making 
+ *  a combined work based on YajHFC. Thus, the terms and conditions of 
+ *  the GNU General Public License cover the whole combination.
+ *  In addition, as a special exception, the copyright holders of YajHFC 
+ *  give you permission to combine YajHFC with modules that are loaded using
+ *  the YajHFC plugin interface as long as such plugins do not attempt to
+ *  change the application's name (for example they may not change the main window title bar 
+ *  and may not replace or change the About dialog).
+ *  You may copy and distribute such a system following the terms of the
+ *  GNU GPL for YajHFC and the licenses of the other code concerned,
+ *  provided that you include the source code of that other code when 
+ *  and as the GNU GPL requires distribution of source code.
+ *  
+ *  Note that people who make modified versions of YajHFC are not obligated to grant 
+ *  this special exception for their modified versions; it is their choice whether to do so.
+ *  The GNU General Public License gives permission to release a modified 
+ *  version without this exception; this exception also makes it possible 
+ *  to release a modified version which carries forward this exception.
  */
 package yajhfc.launch;
 
